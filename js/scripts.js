18c155f15c65c2020b770936b670740c49fa7206
RosarioRu <mariarruval@gmail.com>
2021-10-31 15:44:32 -0700

    Change the code to match epicodus' example code by adding const for 'result'.

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,7 @@ function divide(number1,number2) {
 
 const number1=parseInt(prompt("Enter a number:"));
 const number2=parseInt(prompt("Enter another number:"));
+const result = add(number1,number2);
 
-alert(divide(number1,number2));
+
+alert(result);
